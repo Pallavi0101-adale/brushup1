c61e8eb58786be552e3cf7ef725f002dc1575b08
pallavi0101-adale <pallavi.adale0101@gmail.com>
2024-11-17 14:26:16 +0530

    helloworld

---
+++ helloworld.java
@@ -0,0 +1,12 @@
+public class helloworld {
+    public static void main(String[] args)
+    {
+        int a =30;
+        int b= 20;
+        int c =a-b;
+        System.out.println(c);
+        System.out.println("helloworld");
+        System.out.println("sustraction");
+    }
+}
+
